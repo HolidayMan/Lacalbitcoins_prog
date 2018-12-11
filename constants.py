0ed3a7bc4f6dfde1605d2cfd0cd7c93f6a513e7d
HolidayMan <37782099+HolidayMan@users.noreply.github.com>
2018-12-11 23:41:33 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,6 @@
+class Account:
+	def __init__(self, name, hmac_key, hmac_secret):
+		self.name = name
+		self.hmac_key = hmac_key
+		self.hmac_secret = hmac_secret
+
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from Tkinter import *
 from lbcapi import api
+import constants
 
+accounts = []
+
+with open('accounts.txt','r') as f:
+	for i in f.readlines():
+		a,b,c = i.split('|')
+		accounts.append(constants.Account(a,b,c))
 # gui
 
 root = Tk()
@@ -9,20 +16,15 @@ root.resizable(width=False, height=False)
 root.geometry('1000x600')
 
 frame_top = Frame(root)
+frame_acc = Frame(root)
 frame_center = Frame(root)
-frame_entry = Frame(root)
 frame_bottom= Frame(root)
 
-name = Entry(frame_entry)
-pbkey = Entry(frame_entry)
-sckey = Entry(frame_entry)
 
 inf_lb = Label(frame_top, text='It\'s a localbitcoin program', bg='light green',font=15,width=100)
 
-namelb = Label(frame_entry, text='name:', font=15)
-pbkeylb = Label(frame_entry, text='public key:', font=15)
-sckeylb = Label(frame_entry, text='secret key:', font=15)
 
+add_account = Button(root, text='Add account', width=20, font=15, bg='light blue')
 get_balance = Button(frame_center, text='Balance', width=17)
 show_notif = Button(frame_center, text='Show Notifications', width=17)
 add_notif = Button(frame_center, text='Add Notifications', width=17)
@@ -30,26 +32,23 @@ edit_notif = Button(frame_center, text='Edit Notifications', width=17)
 show_mess = Button(frame_center, text='Show Messages', width=17)
 reply_mess = Button(frame_center, text='Answer Messages', width=17)
 
-answer = Text(frame_bottom, height=17, width=123)
+answer = Text(frame_bottom, height=15, width=98, font=15)
 scrollbar = Scrollbar(frame_bottom)
+
 
 frame_top.pack(side=TOP)
 inf_lb.pack(side=TOP, anchor='center')
-frame_entry.pack(side=TOP, anchor='center', pady=100)
-frame_center.pack(side=TOP, anchor='center', pady=10)
+add_account.pack(pady=100)
 frame_bottom.pack(side=BOTTOM, anchor='center', fill=X)
+frame_center.pack(side=BOTTOM, anchor='center', pady=10)
+
 
 answer.pack(side=LEFT)
 scrollbar.pack(side=RIGHT, fill=Y)
 scrollbar.config(command = answer.yview)
 answer.config(yscrollcomman=scrollbar.set)
 
-namelb.grid(row=0,column=0)
-name.grid(row=0,column=1)
-pbkeylb.grid(row=1,column=0, sticky=E, pady=1)
-pbkey.grid(row=1,column=1, pady=1)
-sckeylb.grid(row=2,column=0, sticky=E, pady=1)
-sckey.grid(row=2,column=1, pady=1)
+
 
 get_balance.grid(row=0, column=0)
 show_notif.grid(row=0, column=1)
@@ -71,5 +70,44 @@ def get_balance_fun(hmac_key,hmac_secret):
 	s = conn.call('GET', '/api/wallet/').json()
 	answer.insert(END,"Balance on {}: {}\n".format(name.get(), s['data']['total']['balance']))
 
+
+def add_account_window(event):
+	add_acc = Toplevel(root)
+	add_acc.title('Adding account')
+	add_acc.resizable(width=False,height=False)
+	add_acc.geometry('300x240')
+	frame_entry = Frame(add_acc)
+
+	name = Entry(frame_entry)
+	pbkey = Entry(frame_entry)
+	sckey = Entry(frame_entry)
+
+	namelb = Label(frame_entry, text='Name:', font=15)
+	pbkeylb = Label(frame_entry, text='Public key:', font=15)
+	sckeylb = Label(frame_entry, text='Secret key:', font=15)
+
+	add = Button(frame_entry, text='Add', font=15, width=10)
+
+	def add_account_fun(event):
+		if name.get() and pbkey.get() and sckey.get():	
+			with open('accounts.txt', 'a') as f:
+				f.write(name.get()+'|'+pbkey.get()+'|'+sckey.get()+'\n')
+			add_acc.destroy()
+
+	add.bind('<Button-1>', add_account_fun)
+
+	frame_entry.pack(side=TOP, anchor='center', pady=50)
+	namelb.grid(row=0,column=0)
+	name.grid(row=0,column=1)
+	pbkeylb.grid(row=1,column=0, sticky=E, pady=1)
+	pbkey.grid(row=1,column=1, pady=1)
+	sckeylb.grid(row=2,column=0, sticky=E, pady=1)
+	sckey.grid(row=2,column=1, pady=1)
+	add.grid(row=3, columnspan=2, pady=10)
+
+
+
+add_account.bind('<Button-1>', add_account_window)
 get_balance.bind('<Button-1>', pre_balance)
+
 root.mainloop()
